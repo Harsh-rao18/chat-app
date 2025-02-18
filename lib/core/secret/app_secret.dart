7f3281b9a04298415a0bd2feb3ebb6b66c43619c
Harsh-rao18 <harshrao64644@gmail.com>
2025-02-18 20:34:18 +0530

    Ignore lib/core/secret folder

--- lib/core/secret/app_secret.dart
+++
@@ -1,5 +0,0 @@
-class AppSecret {
-  static const supabaseUrl = 'https://bycrvamexsfzcpmzpuuo.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5Y3J2YW1leHNmemNwbXpwdXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4NTQ0MzEsImV4cCI6MjA1NTQzMDQzMX0.KS8hQEtQ0fC_cYa-qFxHb_sAR8BKzZzvbB97NJ3rnzc';
-}
